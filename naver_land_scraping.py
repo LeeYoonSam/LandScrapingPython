46b79ff419c8c151e35e7ab88b92d8fd07785b60
Albert.lee <albert.l@cheilpengtai.com>
2018-01-15 16:27:23 +0900

    update scraping - add type, get all pages

--- naver_land_scraping.py
+++ naver_land_scraping.py
@@ -7,10 +7,24 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 import json
 import os
+from enum import Enum
 
-DEF_LAND_URL = "http://land.naver.com/article/articleList.nhn?rletTypeCd=A01&tradeTypeCd=A1&hscpTypeCd=A01&rletNo=110747"
+# DEF_TRADING_LAND_URL = "http://land.naver.com/article/articleList.nhn?rletTypeCd=A01&tradeTypeCd=A1&hscpTypeCd=A01&rletNo=110747"
+# DEF_CHARTER_LAND_URL = "http://land.naver.com/article/articleList.nhn?rletTypeCd=A01&tradeTypeCd=B1&hscpTypeCd=A01&rletNo=110747"
+
+
+DEF_TRADING_LAND_BASE_URL = "http://land.naver.com/article/articleList.nhn?rletTypeCd=A01&tradeTypeCd={}&hscpTypeCd=A01&rletNo=110747"
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+class TradeType(Enum):
+    all = ''            # 전체
+    trading = 'A1'      # 매매
+    charter = 'B1'      # 전세
+    month_rent = 'B2'   # 월세
+    short_rent = 'B3'   # 단기임대
+
+
 class AllLand:
     def __init__(self):
         self.allLand = []
@@ -39,80 +53,111 @@ class LandData:
         self.price = price
         self.contact = contact
 
-def startScraping():
-    r = requests.get(DEF_LAND_URL)
-    soup = BeautifulSoup(r.text, 'lxml')
+def startScraping(scrapingURL, tradeType):
 
-    table = soup.find('table')
-    trs = table.tbody.find_all('tr')
+    newScrapingURL = scrapingURL.format(tradeType.value)
+    count = getPageCount(newScrapingURL)
 
     allLand = AllLand()
 
-    for tr in trs[::2]:
-        tds = tr.find_all('td')
+    for page in range(count):
+        NewURL = newScrapingURL + '&page=' + str(page + 1)
 
-        cols = [' '.join(td.text.strip().split()) for td in tds]
+        r = requests.get(NewURL)
+        soup = BeautifulSoup(r.text, 'lxml')
 
-        if '_thumb_image' not in tds[2]['class']:
-            cols.insert(2, '')
+        table = soup.find('table')
+        trs = table.tbody.find_all('tr')
 
+        # allLand = AllLand()
 
+        for tr in trs[::2]:
+            tds = tr.find_all('td')
 
-        deal = cols[0]
-        date = datetime.strptime(cols[1], '%y.%m.%d.')
+            cols = [' '.join(td.text.strip().split()) for td in tds]
 
-        data3 = cols[3].split(' ')
+            if '_thumb_image' not in tds[2]['class']:
+                cols.insert(2, '')
 
-        if data3[0] == '현장확인':
-            scene_name = data3[0]
-            name = data3[1]
-        else:
-            scene_name = ''
-            name = data3[0]
+            deal = cols[0]
+            date = datetime.strptime(cols[1], '%y.%m.%d.')
 
+            data3 = cols[3].split(' ')
 
-        areas = cols[4]
-
-        supply_area_list = re.findall('공급면적(.*?)㎡', areas)
-        exclusive_list = re.findall('전용면적(.*?)㎡', areas)
-
-        supply_area = 0
-        if len(supply_area_list) > 0:
-            supply_area = supply_area_list[0].replace(',', '')
-
-        exclusive_area = 0
-        if len(exclusive_list) > 0:
-            exclusive_area = exclusive_list[0].replace(',', '')
-
-        supply_area = float(supply_area)
-        exclusive_area = float(exclusive_area)
-
-        dong = cols[5]
-        floor = cols[6]
-
-        try:
-            price = int(cols[7].replace(',', ''))
-        except:
-            price = 0
-
-        contact = cols[8]
-
-        resultDir = os.path.join(BASE_DIR, "Results")
-        if not os.path.exists(resultDir):
-            os.makedirs(resultDir)
-
-        now = datetime.now()
-
-        fileName = now.strftime('%y%m%d%T')
-
-        landData = LandData(deal, date, scene_name, name, supply_area, exclusive_area, dong, floor, price, contact)
-
-        allLand.append_land(landData)
-        # print(json.dumps(landData.__dict__, ensure_ascii=False))
+            if data3[0] == '현장확인':
+                scene_name = data3[0]
+                name = data3[1]
+            else:
+                scene_name = ''
+                name = data3[0]
 
 
-    with open(os.path.join(resultDir, fileName + '_result.json'), 'w+', encoding='utf-8') as json_file:
+            areas = cols[4]
+
+            supply_area_list = re.findall('공급면적(.*?)㎡', areas)
+            exclusive_list = re.findall('전용면적(.*?)㎡', areas)
+
+            supply_area = 0
+            if len(supply_area_list) > 0:
+                supply_area = supply_area_list[0].replace(',', '')
+
+            exclusive_area = 0
+            if len(exclusive_list) > 0:
+                exclusive_area = exclusive_list[0].replace(',', '')
+
+            supply_area = float(supply_area)
+            exclusive_area = float(exclusive_area)
+
+            dong = cols[5]
+            floor = cols[6]
+
+            try:
+                price = int(cols[7].replace(',', ''))
+            except:
+                price = 0
+
+            contact = cols[8]
+
+            resultDir = os.path.join(BASE_DIR, "Results")
+            if not os.path.exists(resultDir):
+                os.makedirs(resultDir)
+
+            now = datetime.now()
+
+            fileName = now.strftime('%y%m%d%T')
+
+            landData = LandData(deal, date, scene_name, name, supply_area, exclusive_area, dong, floor, price, contact)
+
+            allLand.append_land(landData)
+            # print(json.dumps(landData.__dict__, ensure_ascii=False))
+
+
+    with open(os.path.join(resultDir, fileName + "_" + tradeType.name + '_result.json'), 'w+', encoding='utf-8') as json_file:
         json.dump(allLand.get_json_all_land(), json_file, ensure_ascii=False, sort_keys=True, indent=4)
+
+
+def getPageCount(checkUrl):
+    req = requests.get(checkUrl)
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    div_content = soup.find("div", {"class": "paginate"})
+
+    count = 0
+
+    for page in div_content.text:
+        if isNumber(page):
+            count = count + 1
+
+    return count
+
+
+def isNumber(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
 
 
 def html_parser_scraping():
@@ -150,4 +195,6 @@ def html_parser_scraping():
 
 
 # if __name__ == "__main__": html_parser_scraping()
-if __name__ == "__main__": startScraping()
+if __name__ == "__main__":
+    startScraping(DEF_TRADING_LAND_BASE_URL, TradeType.trading)
+    startScraping(DEF_TRADING_LAND_BASE_URL, TradeType.charter)
